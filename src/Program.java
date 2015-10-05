5afa353d0d5abfdb7e51887339ee444871453df1
Ihor I Great <kittoryu@gmail.com>
2015-10-05 21:44:17 +0300

    Error Printing
    Added new error throwing with sysout.

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,7 @@
 public class Program {
 	public static void main(String... args){
 		System.out.println("START PROGRAM");
+		System.out.println("OSHIBKA SHINDOWS 0x0c500000!");
 		System.out.println("Finish Program");
 	}
 }
