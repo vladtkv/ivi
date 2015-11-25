4eb58d52c97f9364ad9ade0dfd573a330e7eb985
Ihor I Great <kittoryu@gmail.com>
2015-11-25 18:13:49 +0200

    Added test of SubscriptionListView method addSubscriptionData

--- ivi/src/test/java/ivi/JUnitTest.java
+++ ivi/src/test/java/ivi/JUnitTest.java
@@ -1,19 +1,26 @@
 package ivi;
 
 import ivi.model.LoginModel;
+import ivi.view.SubscriptionListView;
 import ivi.youtube.GoogleAuthorizer;
 
 import org.junit.*;
+
+import com.google.api.services.youtube.model.Subscription;
+import com.google.api.services.youtube.model.SubscriptionSnippet;
 
 import static org.mockito.Mockito.*;
 
 public class JUnitTest {
 	private static LoginModel lm;
 	private static GoogleAuthorizer googleAuthorizerMock;
+	private Subscription sub;
+	private SubscriptionSnippet subSnip;
+	private SubscriptionListView subListView;
 	@BeforeClass
 	public static void initTest(){
 		googleAuthorizerMock=mock(GoogleAuthorizer.class);
-		lm=new LoginModel(googleAuthorizerMock);	
+		lm=new LoginModel(googleAuthorizerMock);
 	}
 	@Test
 	public void authentificationFailedTest(){
@@ -24,18 +31,13 @@ public class JUnitTest {
 	}
 	
 	@Test
-	public void theBestTestInTheTestWorld(){
-		String s1 = "SKR";
-		String s2 = s1;
-		String s3 = new String("SKR");
-		Assert.assertEquals("SKR", s1);
-		Assert.assertEquals(s1, s2);
-		Assert.assertEquals(s2, s3);
-	}
-	
-	@Test
-	public void testForTest(){
-		int var1 = 6;
-		Assert.assertEquals(1, var1^7);
+	public void addSubscriptionDataTest(){
+		subListView = new SubscriptionListView(null);
+		sub = new Subscription();
+		subSnip = new SubscriptionSnippet();
+		subSnip.setTitle("AWESOME TITLE");
+		sub.setSnippet(subSnip);
+		Assert.assertEquals(false, subListView.addSubscriptionData(null));
+		Assert.assertEquals(true, subListView.addSubscriptionData(sub));
 	}
 }
